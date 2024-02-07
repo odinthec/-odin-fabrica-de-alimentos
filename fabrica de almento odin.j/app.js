cc66f16322de11e05077276356e5f34017c35ac9
odin junior ferdinand <ingresapartlibneg@gmail.com>
2024-02-07 10:57:51 -1200

    fabrica de alimentos
    ventas de alimentos para ganados y aves

---
+++ fabrica de almento odin.j/app.js
@@ -0,0 +1,37 @@
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+});
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    'use strict';
+
+    const switcher = document.querySelector('.btn');
+    
+    switcher.addEventListener('click', function() {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+    
+        const className = document.body.className;
+        if(className == "light-theme") {
+            this.textContent = "Dark";
+        } else {
+            this.textContent = "Light";
+        }
+    
+        console.log('current class name: ' + className);
+    });  
